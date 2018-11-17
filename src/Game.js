88abe5cea229dc35710fa8533348ce26d476ca07
Sam <sam.tuson@hotmail.co.uk>
2018-11-17 18:11:54 +0000

    Change App -> Game

---
+++ src/Game.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './Game.css';
+
+class Game extends Component {
+  render() {
+    return (
+      <div className="game">
+        
+      </div>
+    );
+  }
+}
+
+export default Game;
